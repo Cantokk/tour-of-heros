a71e04d62946c99434872ae8ea882f51c0f20815
Cantokk <jk5643@nyu.edu>
2017-11-21 00:40:52 -0500

    modify

--- angular-tour/src/app/mock-heroes.ts
+++ angular-tour/src/app/mock-heroes.ts
@@ -1,6 +1,6 @@
 import { Hero } from './hero';
 
-export const HEROES: Hero[] = [
+/*export const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
@@ -12,4 +12,13 @@ export const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' },
   { id: 30, name: 'Canto'}
+];*/
+
+export const HEROES:Hero[]=[
+  {id:11,name:'Kang'},
+  {id:11,name:'Ge'},
+  {id:13,name:'Shi'},
+  {id:14,name:'Wo'},
+  {id:15,name:'Er'},
+  {id:16,name:'Zi'}
 ];
